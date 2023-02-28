5acf599930d6cfc63d4fab56a82ad1a4824df2a9
cksdud7007 <84171662+cksdud7007@users.noreply.github.com>
2023-02-28 18:53:13 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,5 @@
+# def solution(numbers):
+#     return  str(int("".join(sorted(list(map(str,numbers)),key = lambda x: x*3,reverse=True))))
+
+def solution(numbers):
+    return str(int("".join(sorted(list(map(str,numbers)),key=lambda x: x *3, reverse=True))))
